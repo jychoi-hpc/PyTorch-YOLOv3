85bfa922c1c7106bcae9252a16e8a07ec18ebcb6
Choi <choij@ornl.gov>
2019-10-17 14:24:45 -0400

    Use aria2

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+if hash aria2c 2>/dev/null; then
+    WGET='aria2c -x 8 -s 8'
+else
+    WGET='wget -c'
+fi
+
 # Download weights for vanilla YOLOv3
-wget -c https://pjreddie.com/media/files/yolov3.weights
+$WGET https://pjreddie.com/media/files/yolov3.weights
 # # Download weights for tiny YOLOv3
-wget -c https://pjreddie.com/media/files/yolov3-tiny.weights
+$WGET https://pjreddie.com/media/files/yolov3-tiny.weights
 # Download weights for backbone network
-wget -c https://pjreddie.com/media/files/darknet53.conv.74
+$WGET https://pjreddie.com/media/files/darknet53.conv.74
