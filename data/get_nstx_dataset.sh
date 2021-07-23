e2043c1525c4de4e1a402647d8c6f395fee7fa3a
Choi <choij@ornl.gov>
2021-07-22 20:52:26 -0400

    minor fix

--- data/get_nstx_dataset.sh
+++ data/get_nstx_dataset.sh
@@ -7,7 +7,7 @@ else
 fi
 
 ## Got an error with aria2
-wget -O nstx.tar.gz https://www.dropbox.com/s/snkp5u65kmh957c/nstx.tar.gz
+wget -O nstx.tar.gz https://www.dropbox.com/s/40gy7wj8stc35au/nstx.tar.gz
 tar xzf nstx.tar.gz
 cd nstx
 python gen_nstx_dataset.py
